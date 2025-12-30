7fc0161b58615b1cff5ddfd738384268a1806837
Alien 1 <Alien 1@192.167.0.77>
2025-12-30 10:27:45 -0600

    Primer commit pokeAPI Service

---
+++ src/main/java/com/digis01/PokeApiService/Configuration/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.digis01.PokeApiService.Configuration;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        
+        dataSource.setUrl("jdbc:oracle:thin:@192.167.1.143:1521:orcl");
+        dataSource.setUsername("PokeAPI25");
+        dataSource.setPassword("password");
+        
+        return dataSource;
+    }
+    
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
+        return new JdbcTemplate(dataSource);
+    }
+    
+}
