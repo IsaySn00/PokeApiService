a70d83608eb2fe3b711588ad2195ddc8fcf17680
IsaySn00 <isaysanchez00@outlook.com>
2026-01-10 22:24:00 -0600

    datasource

--- src/main/java/com/digis01/PokeApiService/Configuration/DataSourceConfig.java
+++ src/main/java/com/digis01/PokeApiService/Configuration/DataSourceConfig.java
@@ -5,24 +5,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class DataSourceConfig {
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setUrl("jdbc:oracle:thin:@192.167.0.104:1521:orcl");
+        
+        dataSource.setUrl("jdbc:oracle:thin:@localhost:1521:orcl");
         dataSource.setUsername("PokeAPI25");
-        dataSource.setPassword("password");
-
+        dataSource.setPassword("password1");
+        
         return dataSource;
     }
-
+    
     @Bean
-    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    public JdbcTemplate jdbcTemplate(DataSource dataSource){
         return new JdbcTemplate(dataSource);
     }
-
+    
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
