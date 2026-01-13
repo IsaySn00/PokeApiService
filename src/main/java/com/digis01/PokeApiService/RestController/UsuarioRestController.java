dfc0e21e003b126b458927b1a90a0c0f3dd8c20a
Alien 1 <Alien 1@192.167.0.77>
2026-01-13 15:55:13 -0600

    Implementación de servicio para recuperar contraseña del usuario

--- src/main/java/com/digis01/PokeApiService/RestController/UsuarioRestController.java
+++ src/main/java/com/digis01/PokeApiService/RestController/UsuarioRestController.java
@@ -8,10 +8,14 @@ import com.digis01.PokeApiService.DAO.UsuarioDAOImplementation;
 import com.digis01.PokeApiService.DTO.UsuarioUpdateDTO;
 import com.digis01.PokeApiService.JPA.Result;
 import com.digis01.PokeApiService.JPA.UsuarioJPA;
+import com.digis01.PokeApiService.Service.EmailService;
+import com.digis01.PokeApiService.Service.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,14 +23,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioRestController {
-    
-     @Autowired
+
+    @Autowired
     private UsuarioDAOImplementation usuarioDAOImplementation;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private EmailService emailService;
 
     @PostMapping()
     public ResponseEntity AddUsuario(@RequestBody UsuarioJPA usuario) {
@@ -48,7 +59,7 @@ public class UsuarioRestController {
 
         return ResponseEntity.status(result.status).body(result);
     }
-    
+
     @PreAuthorize("hasAuthority('ROLE_Administrador') or hasAuthority('ROLE_Usuario')")
     @GetMapping("/{id}")
     public ResponseEntity GetById(@PathVariable("id") int id) {
@@ -88,40 +99,94 @@ public class UsuarioRestController {
 
         return ResponseEntity.status(result.status).body(result);
     }
-    
+
     @GetMapping()
-    public ResponseEntity GetAll(){
+    public ResponseEntity GetAll() {
         Result result = new Result();
-        
-        try{
+
+        try {
             result = usuarioDAOImplementation.GetAll();
             result.correct = true;
             result.status = 200;
-        
-        }catch(Exception ex){
+
+        } catch (Exception ex) {
             result.correct = false;
             result.errorMessage = ex.getLocalizedMessage();
             result.ex = ex;
             result.status = 500;
         }
-        
+
         return ResponseEntity.status(result.status).body(result);
     }
-    
+
     @DeleteMapping("/{idUsuario}")
-    public ResponseEntity DeleteUsuario(@PathVariable("idUsuario") int idUsuario){
+    public ResponseEntity DeleteUsuario(@PathVariable("idUsuario") int idUsuario) {
         Result result = new Result();
-        try{
+        try {
             result = usuarioDAOImplementation.DeleteUsuario(idUsuario);
             result.correct = true;
             result.status = 200;
-            
-        }catch(Exception ex){
+
+        } catch (Exception ex) {
             result.correct = false;
             result.errorMessage = ex.getLocalizedMessage();
             result.ex = ex;
             result.status = 500;
         }
+        return ResponseEntity.status(result.status).body(result);
+    }
+
+    @PostMapping("/sendEmailRecuperacionPass")
+    @PreAuthorize("permitAll()")
+    public ResponseEntity SendEmailRecuperacionPassword(@RequestPart("email") String email) {
+        Result result = new Result();
+
+        try {
+            String tkn = jwtService.generateRecoveryToken(email);
+
+            String link = "http://localhost:8081/usuario/recuperacionContrasenia?token=" + tkn;
+
+            emailService.sendMail(email, link, "Recuperación de contraseña");
+
+            result.correct = true;
+            result.object = "Se ha enviado el correo para recuperar la contraseña";
+            result.status = 200;
+
+        } catch (Exception ex) {
+            result.correct = false;
+            result.errorMessage = ex.getLocalizedMessage();
+            result.ex = ex;
+            result.status = 500;
+        }
+
+        return ResponseEntity.status(result.status).body(result);
+    }
+
+    @PostMapping("recuperarPassword")
+    @PreAuthorize("hasAuthority('ROLE_Invitado')")
+    public ResponseEntity RecuperarPassword(@RequestPart("password") String password) {
+        Result result = new Result();
+
+        try {
+
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+            String email = auth.getName();
+
+            usuarioDAOImplementation.UpdatePassword(email, password);
+            emailService.sendNotification(email, "Recuperación de contraseña");
+
+            result.correct = true;
+            result.object = "La contraseña se recuperó exitosamente";
+            result.status = 200;
+
+        } catch (Exception ex) {
+            result.correct = false;
+            result.errorMessage = ex.getLocalizedMessage();
+            result.status = 500;
+            result.object = "Error al recuperar contraseña";
+        }
+
         return ResponseEntity.status(result.status).body(result);
     }
 }
